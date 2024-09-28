f2ca913ed6aa0a29cf575368da10af0fd4ee60e6
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-09-28 17:57:01 +0700

    change size

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -29,12 +29,14 @@ export default function Port() {
         ]}
       />
       <div className="lg:min-h-screen px-10 sm:px-20 md:px-32 lg:mb-12 lg:px-60 mx-auto max-w-[75rem]">
-        <div className="justify-center mt-20">
+        <div className="justify-center mt-24">
             <h1 className="text-5xl text-center md:text-6xl font-semibold mb-4 lg:text-7xl mb-20">
                 SUBJECTS
             </h1>
         </div>
-        <IconGrid />
+        <div className="w-full flex justify-center">
+          <IconGrid />
+        </div>
       </div>
       
     </>

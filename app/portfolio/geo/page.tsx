4f983d0e9ecaa1647159482d9d7e584324c16b95
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-09-28 17:29:47 +0700

    add margin

--- app/portfolio/geo/page.tsx
+++ app/portfolio/geo/page.tsx
@@ -33,13 +33,13 @@ export default function geo() {
         ]}
       />
       <div className="lg:min-h-screen px-10 sm:px-20 md:px-32 lg:mb-12 lg:px-60 mx-auto max-w-[75rem]">
-        <div className="justify-center mt-20">
-            <h1 className="text-5xl text-center md:text-6xl font-semibold mb-4 lg:text-6xl mb-20">
+        <div className="justify-center mt-24">
+            <h1 className="text-4xl text-center md:text-5xl font-semibold mb-4 lg:text-6xl mb-20">
                 ภูมิศาสตร์
             </h1>
         </div>
 
-        <div className="grid grid-cols-1 gap-10 lg:grid-cols-2">
+        <div className="grid grid-cols-1 gap-10 mt-10 lg:grid-cols-2">
             <div className="flex flex-col p-3 items-center text-center">
                 <iframe
                     className="w-full aspect-video self-stretch md:min-h-96"
